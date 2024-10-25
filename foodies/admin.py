0165fc96bae071af9878f295d3d9dff4c08817ec
Ramees-MT <rameesmullathodika@gmail.com>
2024-10-25 12:00:49 +0530

    first commit

---
+++ foodies/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Registration,Login,Cart,Wishlist,Placeorder
+# Register your models here.
+admin.site.register(Registration)
+admin.site.register(Login)
+admin.site.register(Cart)
+admin.site.register(Wishlist)
+admin.site.register(Placeorder)
+
+
